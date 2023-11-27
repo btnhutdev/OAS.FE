42dd14adfdfe49ca8d217820ec9f3cf5285198d8
btnhutdev <btnhut.dev@gmail.com>
2023-11-27 23:34:32 +0700

    btnhut - remove using unuse

--- Admin/Controllers/ProductController.cs
+++ Admin/Controllers/ProductController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System;
 using System.Threading.Tasks;
